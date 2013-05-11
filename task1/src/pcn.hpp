973c21f972a0f011f2cbaa4e46abf6bc6385bbb9
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-11 19:01:07 +0200

    Swap fetch_and_inc for PCN

--- task1/src/pcn.hpp
+++ task1/src/pcn.hpp
@@ -4,12 +4,18 @@
 #include <atomic>
 #include <iostream>
 
+#include "periodic.hpp"
+
+#define THREAD_COUNT (4)
+
+thread_local int id = -1;
+
 template <class Pheet, typename T>
 class PeriodicCountingNetwork
 {
 public:
     PeriodicCountingNetwork();
-    ~PeriodicCountingNetwork();
+    virtual ~PeriodicCountingNetwork();
 
     void incr();
     T get_sum();
@@ -17,31 +23,45 @@ public:
     static void print_name();
 
 private:
-    std::atomic<T> sum;
+    Periodic *periodic;
+    std::atomic<T> *out;
 };
 
 template <class Pheet, typename T>
-PeriodicCountingNetwork<Pheet, T>::PeriodicCountingNetwork() :
-    sum(0)
+PeriodicCountingNetwork<Pheet, T>::PeriodicCountingNetwork()
 {
+    periodic = new Periodic(THREAD_COUNT);
+    out = new std::atomic<T>[THREAD_COUNT];
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        out[i] = 0;
+    }
 }
 
 template <class Pheet, typename T>
 PeriodicCountingNetwork<Pheet, T>::~PeriodicCountingNetwork()
 {
+    delete periodic;
 }
 
 template <class Pheet, typename T>
 void
 PeriodicCountingNetwork<Pheet, T>::incr()
 {
-    sum++;
+    if (id == -1) {
+        id = Pheet::get_place_id();
+    }
+    out[periodic->traverse(id)]++;
 }
 
 template <class Pheet, typename T>
 T
 PeriodicCountingNetwork<Pheet, T>::get_sum()
 {
+    T sum = 0;
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        sum += out[i].load();
+    }
     return sum;
 }
 
