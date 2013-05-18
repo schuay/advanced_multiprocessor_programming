a5533c3fc9cefb50d02885f5e5739720225f7553
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-18 10:28:16 +0200

    Remove answered question from comments

--- task1/src/pcn.hpp
+++ task1/src/pcn.hpp
@@ -31,9 +31,6 @@ private:
  * What's the point of the counting network if the sum is not returned
  * from incr()? If we need to sum up all values, why not just use one
  * atomic<T> per thread and sum those up? Less processing, better locality.
- *
- * The implementation uses no pheet features and barely any atomics. Is this
- * correct?
  */
 
 template <class Pheet, typename T>
