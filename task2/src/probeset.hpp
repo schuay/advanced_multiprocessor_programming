44f569f8429922b42722d1abd2576fe6f9b5bca9
Jakob Gruber <jakob.gruber@gmail.com>
2013-06-17 14:56:44 +0200

    An empty ProbeSet wrapper

---
+++ task2/src/probeset.hpp
@@ -0,0 +1,13 @@
+#ifndef __PROBESET_H
+#define __PROBESET_H
+
+#define PROBE_THRESHOLD (8)
+#define PROBE_SIZE (16)
+
+template <typename T>
+class ProbeSet {
+public:
+    /* TODO. Wrap std::vector (or similar). */
+};
+
+#endif /* __PROBESET_H */
