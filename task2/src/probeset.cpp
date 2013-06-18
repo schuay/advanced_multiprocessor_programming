af130d0a2e8e97a5cb72d3ef3e273bb45834952c
Jakob Gruber <jakob.gruber@gmail.com>
2013-06-18 23:08:21 +0200

    Implement ProbeSet using std::set

---
+++ task2/src/probeset.cpp
@@ -0,0 +1,33 @@
+#include "probeset.hpp"
+
+template<typename T, class Comparator>
+void
+ProbeSet<T, Comparator>::add(const T &item)
+{
+    the_set.insert(item);
+}
+
+template<typename T, class Comparator>
+bool
+ProbeSet<T, Comparator>::contains(const T &item) const
+{
+    return (the_set.find(item) != the_set.end());
+}
+
+template<typename T, class Comparator>
+void
+ProbeSet<T, Comparator>::remove(const T &item)
+{
+    the_set.erase(item);
+}
+
+template<typename T, class Comparator>
+size_t
+ProbeSet<T, Comparator>::size() const
+{
+    return the_set.size();
+}
+
+template class ProbeSet<
+    unsigned long,
+    std::less<unsigned long> >;
--- task2/src/probeset.hpp
+++ task2/src/probeset.hpp
@@ -1,13 +1,23 @@
 #ifndef __PROBESET_H
 #define __PROBESET_H
 
+#include <cstddef>
+#include <set>
+
 #define PROBE_THRESHOLD (8)
 #define PROBE_SIZE (16)
 
-template <typename T>
+template <typename T,
+          class Comparator = std::less<T> >
 class ProbeSet {
 public:
-    /* TODO. Wrap std::vector (or similar). */
+    void add(const T &item);
+    bool contains(const T &item) const;
+    void remove(const T &item);
+    size_t size() const;
+
+private:
+    std::set<T, Comparator> the_set;
 };
 
 #endif /* __PROBESET_H */
