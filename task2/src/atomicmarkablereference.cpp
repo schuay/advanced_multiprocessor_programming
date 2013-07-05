82117321e48c2ab3f1d25ea8af7a70c94b121407
Jakob Gruber <jakob.gruber@gmail.com>
2013-07-05 15:07:32 +0200

    Move AtomicMarkableReference implementation to cpp

--- task2/src/atomicmarkablereference.cpp
+++ task2/src/atomicmarkablereference.cpp
@@ -1,28 +1,72 @@
-#include "AtomicMarkableReference.hpp"
+#include "atomicmarkablereference.hpp"
 
-
-//template<T>
-AtomicMarkableReference::AtomicMarkableReference()
+AtomicMarkableReference::
+AtomicMarkableReference()
 {
-    static_assert(sizeof(uint32_t) == sizeof(std::thread::id), "This class heavily relies on T being of equal size as uint64_t");
+    static_assert(sizeof(uint64_t) == sizeof(std::thread::id),
+            "This class heavily relies on std::thread::id being "
+            "of equal size as uint64_t");
+    the_value = 0;
 }
 
+AtomicMarkableReference::
+AtomicMarkableReference(std::thread::id reference,
+                        bool mark)
+{
+    static_assert(sizeof(uint64_t) == sizeof(std::thread::id),
+            "This class heavily relies on std::thread::id being "
+            "of equal size as uint64_t");
+    the_value = (uint64_t)mark | *((uint64_t *)&reference) << 1;
+}
 
-//template<T>
 bool
-AtomicMarkableReference::compareAndSet(const std::thread::id expectedReference, const std::thread::id newReference, const bool expectedMark, const bool newMark)
+AtomicMarkableReference::
+compareAndSet(const std::thread::id expectedReference,
+              const std::thread::id newReference,
+              const bool expectedMark,
+              const bool newMark)
 {
-
-
-    return true;
+    return compareAndSet(*((uint64_t *)&expectedReference),
+                         *((uint64_t *)&newReference),
+                         expectedMark,
+                         newMark);
 }
 
-//template<T>
-//T
+bool
+AtomicMarkableReference::
+attemptMark(const std::thread::id newReference,
+            const bool newMark)
+{
+    return compareAndSet(0, *((uint64_t *)&newReference), false, newMark);
+}
+
+void
+AtomicMarkableReference::
+reset()
+{
+    the_value = 0;
+}
+
 std::thread::id
-AtomicMarkableReference::get(const bool *)
+AtomicMarkableReference::
+get(bool *mark)
 {
-    return NULL;
+    *mark = the_value & 1;
+    uint64_t val = (the_value >> 1);
+    return  ((std::thread::id)val);
 }
 
-//template class AtomicMarkableReference<T>;
+bool
+AtomicMarkableReference::
+compareAndSet(const uint64_t expectedReference,
+              const uint64_t newReference,
+              const bool expectedMark,
+              const bool newMark)
+{
+    uint64_t expected, update;
+
+    //TODO: casting std::thread::id to uint64_t might be dangerous!! std::thread::id does not give any guarantees except for it to be unique for each thread.
+    expected = (uint64_t)expectedMark | expectedReference << 1;
+    update = (uint64_t)newMark | newReference << 1;
+    return the_value.compare_exchange_strong(expected, update);
+}
