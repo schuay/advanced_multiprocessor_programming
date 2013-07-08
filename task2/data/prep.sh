f85c91008725da53f2e6b098d044f1e968eafad0
Jakob Gruber <jakob.gruber@gmail.com>
2013-07-08 18:56:58 +0200

    Squelch rm errors

--- task2/data/prep.sh
+++ task2/data/prep.sh
@@ -1,5 +1,5 @@
-rm 1_*.dat
-rm 2_*.dat
+rm -f 1_*.dat
+rm -f 2_*.dat
 for f in *.dat
 do
    sed -e '/test/d' -n -e 's/.*[\ \t]\(0.5	0.25\)[\ \t]0[\ \t]\([0-9]*[\ \t][0-9.]*\)[\ \t]*$/\2/p' $f | sort -g -k1 -k2 | sort -gu -k1 >1_$f
--- task2/protocol/gp.sh
+++ task2/protocol/gp.sh
@@ -1,5 +1,5 @@
-rm *.pdf
-rm *.eps
+rm -f *.pdf
+rm -f *.eps
 
 for f in *.gp; do
 	gnuplot $f;
